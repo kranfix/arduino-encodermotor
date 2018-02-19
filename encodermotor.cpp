80c095da76c9f31f02521f0a9a6ca6881c4b17bc
Frank Moreno <frankmoreno1993@gmail.com>
2018-02-19 10:55:00 -0500

    library: missing some functionalities

--- encodermotor.cpp
+++ encodermotor.cpp
@@ -1,32 +1,35 @@
+#include <Arduino.h>
 #include "encodermotor.h"
- 
-MotorEncoder::MotorEncoder(byte encoderPin,int N, int radius
+
+EncoderMotor::EncoderMotor(uint8_t encoderPin,int N, int radius,
                frontback_t motorPin){
   this->encoderPin = encoderPin;
   this->N = N;
   this->radius = radius;
-  
+
   this->motorPin   = motorPin;
 
   pinMode(encoderPin,    INPUT);
   pinMode(motorPin.front,OUTPUT);
   pinMode(motorPin.back, OUTPUT);
-  
+
+  last = digitalRead(encoderPin);
+
   setMotor(Action::Stop,0);
 }
 
-void MotorEncoder::setLinearPosition(int pos){
-  
+void EncoderMotor::setLinearPosition(float pos){
+
 }
 
-void MotorEncoder::setMotor(Action _act){
+void EncoderMotor::setMotor(Action _act, float limit){
   act = _act;
   digitalWrite(motorPin.front, act == Action::Forward);
   digitalWrite(motorPin.back,  act == Action::Backward);
 }
 
-void MotorEncoder::loop(){
-  byte now = digitalRead(encoderPin);
+void EncoderMotor::loop(){
+  uint8_t now = digitalRead(encoderPin);
   if(last != now){
     if(act == Action::Forward){
       encoderPosition++;
@@ -36,6 +39,11 @@ void MotorEncoder::loop(){
   }
   last = now;
 }
+
+bool EncoderMotor::encoderEvent(){
+  return event;
+}
+
 
 //eventBack = (analogRead(irPin.back) >= irLevel);
 //eventFront = (analogRead(irPin.front) >= irLevel);
---
+++ encodermotor.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <stdint.h>
+
+enum class Action{Stop,Forward,Backward};
+
+typedef struct {
+  uint8_t front;
+  uint8_t back;
+} frontback_t;
+
+class EncoderMotor{
+  // Encoder
+  uint8_t encoderPin;
+  int encoderPosition;
+  int limit;
+  int N; // number or slots
+  float radius;
+  uint8_t last; // last encoder read: 0 or 1
+  uint8_t event;
+
+  // Motor
+  frontback_t motorPin;
+  Action act;
+public:
+  EncoderMotor(uint8_t encoderPin,int N, int radius, frontback_t motorPin);
+  void setLinearPosition(float pos);
+  void setMotor(Action act, float limit);
+  void loop();
+  bool encoderEvent();
+};
